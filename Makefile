0eb44c33e8fc4d2dd30ff44f2c269c94907d5543
James Reed <jamesr66@vt.edu>
2014-03-13 05:48:55 -0400

    Generalized code and added TERM signal handling

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
+CFLAGS:=--std=c89 -Wall -pedantic
+
 fade: src/fade.c
-	distcc armv6j-hardfloat-linux-gnueabi-gcc -o obj/fade.o -c src/fade.c
-	gcc --std=c89 -Wall -pedantic -o ./fade obj/fade.o -pthread -lwiringPi
+	cpp $(CPPFLAGS) src/fade.c -o temp/fade.c
+	distcc armv6j-hardfloat-linux-gnueabi-gcc -o obj/fade.o -c temp/fade.c
+	gcc $(CFLAGS) -o ./fade obj/fade.o -pthread -lwiringPi
 
 clean:
-	rm -f ./fade obj/*
+	rm -f ./fade obj/* temp/*
--- src/fade.c
+++ src/fade.c
@@ -2,29 +2,62 @@
 #include <softPwm.h>
 #include <assert.h>
 #include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
 
+/* Define preprocessor directive LED if you want to test with
+ * visible LED brightness range and oscillation period */
+
+#ifndef LED
 static const unsigned int PERIOD = 100000;
 static const unsigned int MIN = 10;
 static const unsigned int MAX = 20;
+#else
+static const unsigned int PERIOD = 10000;
+static const unsigned int MIN = 0;
+static const unsigned int MAX = 100;
+#endif
+
+/* Determine which pin to output PWM on */
+static const unsigned int PIN = 14;
+
+
+/* signal TERM handler */
+void term_handler(int signal)
+{
+	usleep(PERIOD);
+	softPwmWrite(PIN, 0);
+	exit(0);
+}
 
 int main()
 {
+	/*setup interrupt handler to break out of loop */
+	struct sigaction sa;
+	sa.sa_handler = term_handler;
+	
+	sigaction( SIGINT, &sa, NULL );
+
+	/* setup for the PWM signal on the specified
+	   wiringPi pin */
+
 	int counter = 0;
 	wiringPiSetup();
 
-	int status = softPwmCreate(1, 0, 100);
+	int status = softPwmCreate(PIN, 0, 100);
 	assert(!status);
-
+	
+	/* infinite loop to fade signal across range */
 	while (1)
 	{
 		for (counter = MIN; counter < MAX; counter++)
 		{
-			softPwmWrite(1, counter);
+			softPwmWrite(PIN, counter);
 			usleep(PERIOD);
 		}
-		for (counter = MAX; counter >= MIN; counter--)
+		for (counter = MAX; counter > MIN; counter--)
 		{
-			softPwmWrite(1, counter);
+			softPwmWrite(PIN, counter);
 			usleep(PERIOD);
 		}
 	}
